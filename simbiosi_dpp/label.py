a8ea487ad9d5cef04f52883edb6132cfccc7e795
migo <1695587905@qq.com>
2025-05-01 04:41:26 +0000

    数据处理

---
+++ simbiosi_dpp/label.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+import pandas as pd
+from tqdm import tqdm
+
+tqdm.pandas()
+speakers = {}
+
+
+def get_person(object_name: str):
+    speaker_name = object_name.split("/")[-2]
+    return speaker_name
+
+
+def person_to_id(speaker_name: str):
+    global speakers
+    if speaker_name not in speakers:
+        speakers[speaker_name] = len(speakers)
+    return speakers[speaker_name]
+
+
+def main():
+    tsv_path = Path("data-bin/all.tsv")
+    output_path = Path("data-bin/all_with_ids.tsv")
+    df = pd.read_csv(tsv_path, sep="\t")
+    df["person"] = df["object_name"].progress_apply(get_person)
+    df["id"] = df["person"].progress_apply(person_to_id)
+    df = df.drop(columns=["md5", "size"])
+    print(df.head())
+    df.to_csv(output_path, sep="\t", index=False)
+
+
+if __name__ == "__main__":
+    main()
